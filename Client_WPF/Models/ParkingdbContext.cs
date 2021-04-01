746c01899e41d60bbf5e97e270a8290fc7214e8d
Grawter <terminator17067@mail.ru>
2021-04-01 20:40:06 +0300

    Добавление WPF клиента. Реализация первой вкладки "Список"

---
+++ Client_WPF/Models/ParkingdbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Web_Service.Models;
+
+namespace Client_WPF.Models
+{
+    public partial class ParkingdbContext : DbContext
+    {
+        public ParkingdbContext()
+        {
+        }
+
+        public ParkingdbContext(DbContextOptions<ParkingdbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<ParkingInfo> Parks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            }
+        }
+
+    }
+}
---
+++ Client_WPF/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Web_Service.Models;
+using Client_WPF.Models;
+
+namespace Client_WPF.ViewModels
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public ApplicationViewModel()
+        {
+            db = new ParkingdbContext();
+            dbSize = db.Parks.Count();
+
+            Parks = new ObservableCollection<ParkingInfo>();
+        }
+
+        private readonly ParkingdbContext db;
+        public int dbSize { get; private set; }
+        public ObservableCollection<ParkingInfo> Parks { get; set; }
+
+        private ParkingInfo selectedPark;
+        public ParkingInfo SelectedPark
+        {
+            get { return selectedPark; }
+            set
+            {
+                selectedPark = value;
+                OnPropertyChanged("SelectedPark");
+            }
+        }
+
+        // команда подгрузки данных в список
+        private RelayCommand downCommand;
+        public RelayCommand DownCommand
+        {
+            get
+            {
+                return downCommand ??
+                  (downCommand = new RelayCommand(obj =>
+                  {
+                      int sizeList = (int)obj;
+
+                      if (sizeList == 0)
+                          sizeList = 1;
+
+                      if (sizeList + 10 >= dbSize)
+                      {
+                          for (int i = sizeList; i < dbSize; i++)
+                          {
+                              Parks.Add(db.Parks.FirstOrDefault(p => p.Id == i));
+                          }
+                      }
+                      else
+                      {
+                          for (int i = sizeList; i < sizeList + 10; i++)
+                          {
+                              Parks.Add(db.Parks.FirstOrDefault(p => p.Id == i));
+                          }
+                      }
+                  },
+                 (obj) => Parks.Count < dbSize));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
